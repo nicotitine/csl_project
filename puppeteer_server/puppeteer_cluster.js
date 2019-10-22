ba73320b65f9b18946f134ca25f929a82a10a072
Nicolas <nico.hincelin@gmail.com>
2019-10-22 23:05:53 +0200

    Search function added. Product categorization added.

--- puppeteer_server/puppeteer_cluster.js
+++ puppeteer_server/puppeteer_cluster.js
@@ -32,7 +32,8 @@ const main = async () => {
      */
     cluster = await Cluster.launch({
         concurrency: Cluster.CONCURRENCY_BROWSER,
-        maxConcurrency: cpuCount
+        maxConcurrency: cpuCount,
+        puppeteerOptions: { args: ['--no-sandbox'] }
     });
 
     console.log(`Puppeteer cluster launched with ${cpuCount} worker(s) and ${delay} ms of delay on port ${process.env.PUPPETEER_PORT}`);
